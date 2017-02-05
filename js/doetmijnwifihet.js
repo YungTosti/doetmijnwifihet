7bd928c05e303cbae90f455d007a3dc34896db66
Bram Versluis <harmjan.versluis@gmail.com>
2017-02-05 23:35:48 +0100

    Added way more text and some cool animations in jQuery

---
+++ js/doetmijnwifihet.js
@@ -0,0 +1,67 @@
+/**
+ * Created by yungtosti on 05/02/17.
+ */
+
+window.onload = executeAnimation();
+var inSubAnimation = false;
+var executedConnectionBarAnimation = false;
+var globalCounter = 1; // Deze nepnerd begon vanaf 1 te tellen
+
+function executeAnimation() {
+    setInterval(function() {
+        if (inSubAnimation) return;
+
+        toggleLineVisibility(globalCounter);
+
+        if (globalCounter === 6 && !executedConnectionBarAnimation) {
+            executeConnectionBarAnimation();
+        }
+        else globalCounter++;
+    }, 1500);
+}
+
+function toggleLineVisibility(lineNumber) {
+    jQuery('h1.line-' + lineNumber).toggleClass('invisible');
+}
+
+function executeConnectionBarAnimation() {
+    inSubAnimation = true;
+    executedConnectionBarAnimation = true;
+    var counter = 1;
+    var rounds = 0;
+    var maxRounds = 2;
+    var positive = true;
+    var justSwitched = false;
+
+    setInterval(function() {
+        justSwitched = false;
+
+        if (counter === 5 && positive && rounds < maxRounds) {
+            positive = false;
+            justSwitched = true;
+        }
+
+        if (counter === 0 && !positive && rounds < maxRounds) {
+            positive = true;
+            justSwitched = true;
+            rounds++;
+        }
+
+        toggleConnectionBarVisibility(counter);
+
+        if (counter === 7) {
+            //We hebben de online cloud laten zien, we kunnen terug naar de normale loop
+            inSubAnimation = false;
+            globalCounter++;
+            return;
+        }
+
+        if (justSwitched) return;
+        if (positive) counter++;
+        else counter--;
+    }, 100);
+}
+
+function toggleConnectionBarVisibility(lineNumber) {
+    jQuery('.connection .wifi-' + lineNumber).toggleClass('transparent');
+}
